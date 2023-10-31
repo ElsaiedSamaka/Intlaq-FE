e66884ab67ab8900ae2ae48132e5b42661d35d3d
Saied <elsaied.samaka1997@gmail.com>
2023-10-31 19:03:42 +0200

    code refactor

--- src/app/pages/home/components/jobs/views/views.module.ts
+++ src/app/pages/home/components/jobs/views/views.module.ts
@@ -1,12 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { JobCardComponent } from './job-card/job-card.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [JobCardComponent],
-  exports: [
-     JobCardComponent
-   ]
+  exports: [JobCardComponent],
 })
 export class ViewsModule {}
